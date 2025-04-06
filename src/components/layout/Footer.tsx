0560188b7c6c68040145bf21e824a388eca3cce5
Tetiana Mostova <mosttn18@gmail.com>
2025-04-06 16:22:33 -0600

    Fix lint

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
 import { Link } from 'react-scroll';
 import { theme } from '../../styles/theme';
 
